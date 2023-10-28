a006ea29202176ebaab2ce2b980b008ba09833ba
NAMO <81159848+skah1061@users.noreply.github.com>
2023-10-28 20:17:44 +0900

    Update 수들의 합 2.java

--- 백준/Silver/2003. 수들의 합 2/수들의 합 2.java
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.java
@@ -15,27 +15,27 @@ public class Main {
     int[] numArray = new int[N];
 
     st = new StringTokenizer(br.readLine());
-    for (int i = 0; i < N; i++) {
+    for (int i = 0; i < N; i++) { //배열 초기화
       numArray[i] = Integer.parseInt(st.nextToken());
     }
-    int cnt = 0;
+    //투포인터 사용
+    int cnt = 0;  //M과 같은 경우의 수
     int result = 0; //i ~ j 까지의 합 0으로 초기화
-    int start = 0;
-    int end = 0;
+    int start = 0;  //시작 포인트
+    int end = 0;  //끝 포인트
     while (true) {
-      if (result >= M) {
+      if (result >= M) {  // 값이 목표 값보다 커지면 start번째 값을 빼고 시작포인트를 하나 오른쪽으로 옮긴다
         result -= numArray[start];
         start++;
-      } else if (end == N) {
+      } else if (end == N) {  //값이 목표보다 크지 않고 끝 표인트가 N에 도달하면 종료
         break;
-      } else {
+      } else {  //end번째 값만큼 더해주고 끝 포인트 한칸 이동
         result += numArray[end];
         end++;
       }
       if (result == M) {
         cnt++;
       }
-
     }
     System.out.println(cnt);
   }
